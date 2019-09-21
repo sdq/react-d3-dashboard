c7bf30637e17b0ac82ea699db5a52a97dbdc853c
sdq <sdq@tongji.edu.cn>
2019-09-22 02:18:07 +0800

    barchart demo

--- src/charts/BarChart/vis.js
+++ src/charts/BarChart/vis.js
@@ -1,10 +1,50 @@
 import * as d3 from 'd3';
+import data from '../../data/barchart-demo';
+import './style.css';
 
 const draw = (props) => {
     d3.select('.vis-barchart > *').remove();
-    const width = props.width;
-    const height = props.height;
-    d3.select('.vis-barchart').append('svg').attr('width',width).attr('height',height)
+    const margin = {top: 20, right: 20, bottom: 30, left: 40};
+    const width = props.width - margin.left - margin.right;
+    const height = props.height - margin.top - margin.bottom;
+    let svg = d3.select('.vis-barchart').append('svg')
+            .attr('width',width + margin.left + margin.right)
+            .attr('height',height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // format the data
+    data.forEach(function(d) {
+        d.age = +d.age;
+    });
+
+    // Scale the range of the data in the domains
+    let x = d3.scaleBand()
+          .range([0, width])
+          .padding(0.1);
+    let y = d3.scaleLinear()
+          .range([height, 0]);
+    x.domain(data.map(function(d) { return d.name; }));
+    y.domain([0, d3.max(data, function(d) { return d.age; })]);
+
+    // append the rectangles for the bar chart
+    svg.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.name); })
+        .attr("width", x.bandwidth())
+        .attr("y", function(d) { return y(d.age); })
+        .attr("height", function(d) { return height - y(d.age); });
+
+    // add the x Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // add the y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
 }
 
 export default draw;
--- src/views/View5/index.js
+++ src/views/View5/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import BarChart from '../../charts/BarChart';
 import './view5.css';
 
 export default class View5 extends Component {
     render() {
         return (
-            <div id='view5' className='pane'>
+            <div id='view5' className='pane' style={{padding: 10}}>
                 view 5
+                <BarChart width={1000} height={560}/>
             </div>
         )
     }
