4f371624a03bc953616e6850820d3b5711a98617
CindyXu77 <675984613@qq.com>
2019-09-22 15:35:13 +0800

    finish pie and line

--- src/views/View2/index.js
+++ src/views/View2/index.js
@@ -4,8 +4,8 @@ import PieChart from '../../charts/PieChart';
 
 export default class View2 extends Component {
     render() {
-        const width = 200
-        const height = 200
+        const width = 260
+        const height = 260
         return (
             <div id='view2' className='pane'>
                 <div className='header'>view 2</div>
--- src/views/View4/index.js
+++ src/views/View4/index.js
@@ -4,12 +4,14 @@ import LineChart from '../../charts/LineChart';
 
 export default class View4 extends Component {
     render() {
-        const width = 900
-        const height = 250
+        const width = 1100,
+              height = 250
         return (
-            <div id='view4' className='pane'>
+            <div id='view4' className='pane' >
                 <div className='header'>view 4</div>
-                <LineChart width={width} height={height}/>
+                <div style={{ overflowX: 'scroll',overflowY:'hidden' }}>
+                    <LineChart width={width} height={height} />
+                </div>
             </div>
         )
     }
