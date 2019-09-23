79b49ad8a79d90f2ae70add5689345f26a1ad6b0
sdq <sdq@tongji.edu.cn>
2019-09-23 16:10:12 +0800

    add docs

---
+++ docs/precache-manifest.8a7c85036b5b73a825f10d44b1175752.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7c113512e347e0e6edbb31e1a96285",
+    "url": "/index.html"
+  },
+  {
+    "revision": "894fbc697292a88cecbb",
+    "url": "/static/css/2.96121624.chunk.css"
+  },
+  {
+    "revision": "479f5f6fca4a97ac6251",
+    "url": "/static/css/main.a9af0b1c.chunk.css"
+  },
+  {
+    "revision": "894fbc697292a88cecbb",
+    "url": "/static/js/2.446796eb.chunk.js"
+  },
+  {
+    "revision": "479f5f6fca4a97ac6251",
+    "url": "/static/js/main.e893ceda.chunk.js"
+  },
+  {
+    "revision": "02c5cf49242e002836ea",
+    "url": "/static/js/runtime~main.c277b7a9.js"
+  }
+]);
